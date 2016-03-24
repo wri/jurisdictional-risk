43fb0191fc2f1d975f6673e9c7ec2cf65789b5c6
Juer Song <CSong@wri.org>
2016-03-24 17:21:28 -0400

    Update district_level_risk_analysis.py

--- district_level_risk_analysis.py
+++ district_level_risk_analysis.py
@@ -5,10 +5,7 @@ import numpy
 import ee
 from scipy import stats
 
-ee.Initialize()
-
-#APIURL = 'http://wri-01.cartodb.com/api/v2/sql'
-#APIKEY = '0e5365cb1a299778e9df9c7bf6db489af8aa08e1'
+"""CartoDB API authentication and GFW url endpoint"""
 GFW_APIURL = 'http://staging.gfw-apis.appspot.com/forest-change'
 
 APIURL = 'http://wri-02.cartodb.com/api/v2/sql'
@@ -19,13 +16,13 @@ APIKEY = '437bccaeec274664457746f6299d929f4e35bbb9'
 
 #protected area has fields that record tree cover loss in 2001-2013, sum of loss from 2001-2013, and fields that record the change rate from 2009-2013
 
-def window(table):
+def window(table): #applies the moving window method to the table.
 	
 	n = 441 #441 districts in Indonedsia, id_2 values ranging from 1 to 441
 	for id_2 in range(1, n+1):
 		print id_2
 		i = 2
-		while(i <= 12):
+		while(i <= 12):#average loss from year 2002(avg of 2001,2002,2003) to 2012(avg of 2011,2012,2013)
 			b = str(2000 + i)
 			a = str(2000 + i - 1)
 			c = str(2000 + i + 1)
@@ -35,10 +32,11 @@ def window(table):
 			fa = 'total_loss_%s'% a
 			#fc = 'loss_%s'% c
 			fc = 'total_loss_%s'% c
+			#For a specific year, get the loss of that year, as well as the loss of before and after years.
 			loss_a = get_intersect_values(table,fa,'adm_id', id_2)[0][fa]
 			loss_b = get_intersect_values(table,fb,'adm_id', id_2)[0][fb]
 			loss_c = get_intersect_values(table,fc,'adm_id', id_2)[0][fc]
-			window_b = 'window_total_%s'% b
+			window_b = 'window_total_%s'% b #
 			print loss_a
 			print loss_b
 			print loss_c
@@ -46,13 +44,14 @@ def window(table):
 				i+=1
 				continue
 			else:
-				avg = (loss_a + loss_b + loss_c)/3
+				avg = (loss_a + loss_b + loss_c)/3 #Calculate the average.
 				print avg
-				#update_table('idn_adm2_risk_analysis', window_b, avg,id_2, 'id_2')
+				update_table('idn_adm2_risk_analysis', window_b, avg,id_2, 'id_2') #Update the table.
 				i+=1
 
 
-
+# For each of the 441 districts, this function 1)firstly applies the moving window method to the tree cover loss, 
+# and 2) then fits a linear regression line to the moving-window-applied loss, and updates the table with the slope, r-squared, and p-value.
 def regression(table, id_fieldname):
 	n = 441
 	for id_2 in range(1, n+1):
@@ -91,11 +90,11 @@ def regression(table, id_fieldname):
 			gradient, intercept, r_value, p_value, std_err = stats.linregress(x,y)
 			print "Gradient, ntercept, R-squared, std_err", gradient, intercept, r_value**2, std_err
 			FieldInRiskTable = 'rate_total_loss'
-			update_table('idn_adm2_risk_analysis', FieldInRiskTable, gradient,id_2, 'id_2')
-			update_table('idn_adm2_risk_analysis', 'p_value_total_loss', p_value,id_2, 'id_2')
-			update_table('idn_adm2_risk_analysis', 'r_squared_total_loss', r_value**2,id_2, 'id_2')
+			update_table('idn_adm2_risk_analysis', FieldInRiskTable, gradient,id_2, 'id_2') #Update the table with the slope of the fitting line
+			update_table('idn_adm2_risk_analysis', 'p_value_total_loss', p_value,id_2, 'id_2') #Update the table with the p-value of the coefficient
+			update_table('idn_adm2_risk_analysis', 'r_squared_total_loss', r_value**2,id_2, 'id_2')#Update the table with the r-square of the regression
 
-
+#This function does the same thing with the function above, just tweaked to fit specific fields of the table.
 def regression_window(table):
 	
 	#n = 441 #441 districts in Indonedsia, id_2 values ranging from 1 to 441
@@ -149,6 +148,7 @@ def regression_window(table):
 			update_table(table, 'p_value_primary_loss', p_value,cartodb_id, 'cartodb_id')
 			update_table(table, 'r_squared_primary_loss', r_value**2,cartodb_id, 'cartodb_id')
 
+# For each of the 441 districts, calculate the percentage area of the intersection with a layer(intersected area with protected/primary/ over the entire area of district)
 def summarize_loss_in_intersec(intersec_table, FieldOfInterest, FieldInRiskTable):
 	n = 441 #441 districts in Indonedsia, id_2 values ranging from 1 to 441
 	for id_2 in range(1, n+1):
@@ -166,96 +166,8 @@ def summarize_loss_in_intersec(intersec_table, FieldOfInterest, FieldInRiskTable
 		
 		update_table('idn_adm2_risk_analysis', FieldInRiskTable, perc,id_2, 'id_2')
 
-def rate_loss_in_protected():
-	intersec_table = 'idn_adm2_protected'
-	n = 441 #441 districts in Indonedsia, id_2 values ranging from 1 to 441
-	for id_2 in range(1,n+1):
-		print id_2
-		intersect_area_list = get_intersect_values(intersec_table,'area_hectare','adm_id', id_2)
-		if len(intersect_area_list)==0:
-			continue
-		else:
-
-
-			total_protected_area = sum_value(intersect_area_list)
-
-			"""Calculate tree cover loss in protected areas in 2001-2007"""
-
-			intersect_loss_2001_list = get_intersect_values(intersec_table, 'loss_2001', 'adm_id', id_2)
-			total_loss_2001 = sum_value(intersect_loss_2001_list)
-
-			intersect_loss_2002_list = get_intersect_values(intersec_table, 'loss_2002', 'adm_id', id_2)
-			total_loss_2002 = sum_value(intersect_loss_2002_list)
-
-			intersect_loss_2003_list = get_intersect_values(intersec_table, 'loss_2003', 'adm_id', id_2)
-			total_loss_2003 = sum_value(intersect_loss_2003_list)
-
-			intersect_loss_2004_list = get_intersect_values(intersec_table, 'loss_2004', 'adm_id', id_2)
-			total_loss_2004 = sum_value(intersect_loss_2004_list)
-
-			intersect_loss_2005_list = get_intersect_values(intersec_table, 'loss_2005', 'adm_id', id_2)
-			total_loss_2005 = sum_value(intersect_loss_2005_list)
-
-			intersect_loss_2006_list = get_intersect_values(intersec_table, 'loss_2006', 'adm_id', id_2)
-			total_loss_2006 = sum_value(intersect_loss_2006_list)
-
-			intersect_loss_2007_list = get_intersect_values(intersec_table, 'loss_2007', 'adm_id', id_2)
-			total_loss_2007 = sum_value(intersect_loss_2007_list)
-
-			update_table('idn_adm2_risk_analysis', 'loss_2001', total_loss_2001, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2002', total_loss_2002, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2003', total_loss_2003, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2004', total_loss_2004, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2005', total_loss_2005, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2006', total_loss_2006, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2007', total_loss_2007, id_2,'id_2')
-
-			"""Calculate tree cover loss in protected areas in 2008-2013"""
-
-			intersect_loss_2008_list = get_intersect_values(intersec_table, 'loss_2008', 'adm_id', id_2)
-			total_loss_2008 = sum_value(intersect_loss_2008_list)
-
-			intersect_loss_2009_list = get_intersect_values(intersec_table, 'loss_2009', 'adm_id', id_2)
-			total_loss_2009 = sum_value(intersect_loss_2009_list)
-
-			intersect_loss_2010_list = get_intersect_values(intersec_table, 'loss_2010', 'adm_id', id_2)
-			total_loss_2010 = sum_value(intersect_loss_2010_list)
-
-			intersect_loss_2011_list = get_intersect_values(intersec_table, 'loss_2011', 'adm_id', id_2)
-			total_loss_2011 = sum_value(intersect_loss_2011_list)
-
-			intersect_loss_2012_list = get_intersect_values(intersec_table, 'loss_2012', 'adm_id', id_2)
-			total_loss_2012 = sum_value(intersect_loss_2012_list)
-
-			intersect_loss_2013_list = get_intersect_values(intersec_table, 'loss_2013', 'adm_id', id_2)
-			total_loss_2013 = sum_value(intersect_loss_2013_list)
-
-			update_table('idn_adm2_risk_analysis', 'loss_2008', total_loss_2008, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2009', total_loss_2009, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2010', total_loss_2010, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2011', total_loss_2011, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2012', total_loss_2012, id_2,'id_2')
-			update_table('idn_adm2_risk_analysis', 'loss_2013', total_loss_2013, id_2,'id_2')
-			"""
-
-			try:
-
-				change_rate09 = (total_loss_2009-total_loss_2008)/total_loss_2008
-			
-				change_rate10 = (total_loss_2010-total_loss_2009)/total_loss_2009
-			
-				change_rate11 = (total_loss_2011-total_loss_2010)/total_loss_2010
-			
-				change_rate12 = (total_loss_2012-total_loss_2011)/total_loss_2011
-			
-				change_rate13 = (total_loss_2013-total_loss_2012)/total_loss_2012
-
-				avg_change_rate = numpy.mean([change_rate09, change_rate10, change_rate11, change_rate12, change_rate13])
-				update_table('idn_adm2_risk_analysis', 'rate_of_loss_in_protected_areas', avg_change_rate, id_2,'id_2')
-
-			except ZeroDivisionError:
-				continue"""
-
+#Intersec_table is the intersection layer of district table and protected/primary forest, overlapping_table is the intersection layer of district table, protected/primary forest, and palm oil concessions
+#This function calculates the porprotion of concession area over protected/primary forest, per district
 def palm_concession_in_protected(intersec_table,overlapping_table,FieldOfInterest,FieldInRiskTable):
 	
 	n = 441
@@ -291,7 +203,7 @@ def update_table(table, field, value, identifier, id_fieldname):
 	r = requests.get(APIURL, params=dict(api_key=APIKEY, q=query))
 	
 
-
+#This function gets the list of values of a field for a specific where_value from CartoDB
 def get_intersect_values(table, fieldname, where_name, where_value):	
 	query = """
 			SELECT %s FROM %s where %s = %s""" % (fieldname, table, where_name, where_value)
@@ -299,13 +211,15 @@ def get_intersect_values(table, fieldname, where_name, where_value):
 	value_list = r.json()['rows']
 	return value_list
 
+#This function gets the list of ids from the table on CartoDB
 def get_all_ids(table, id_fieldname):	
 	query = """
 			SELECT %s FROM %s""" % (id_fieldname, table)
 	r = requests.get(APIURL, params=dict(api_key=APIKEY, q=query))
 	id_list = r.json()['rows']
 	return id_list
-
+	
+#This function gets the geometry of a polygon
 def get_geoms(intersect_id, id_fieldname, table):
     query = """
             SELECT the_geom FROM %s
@@ -316,7 +230,8 @@ def get_geoms(intersect_id, id_fieldname, table):
     geoms = r.json()['features'][0]['geometry']
     strgeom = json.dumps(geoms)
     return strgeom
-
+    
+#This function gets the simple geometry of a polygon. This is used only when the get_geoms function causes error due to the complexity of the polygon
 def get_simple_geoms(intersect_id, id_fieldname, table):
     
     query = """
@@ -336,6 +251,7 @@ def get_simple_geoms(intersect_id, id_fieldname, table):
         
         return None
 
+#Adding up the areas
 def sum_value(valuelist):
 	total_value = 0
 	for next_value in valuelist:
@@ -343,6 +259,7 @@ def sum_value(valuelist):
 		total_value += value
 	return total_value
 
+#Using GFW API to calculate tree cover loss/forma alerts. Documentations on https://github.com/wri/gfw-api
 def gfw_api_request(geom, start_date, end_date, dataset, threshold=30):
     """Make GFW API request for a given geom to calculate the loss in a
     given area."""
@@ -378,162 +295,19 @@ def sum_values(polygon_id,idfieldname, district_id,table):
 		total_area = total_area + area
 	return total_area
 
-def loss_in_geom(table, start_date, end_date, d):
-	#table = 'idn_adm2_protected'
-	#id_fieldname = 'intersect_id'
-	id_fieldname = 'adm_id'
-	
-	ids = get_all_ids(table, id_fieldname)
-	print ids
-	orange = 0
-	for intersection in ids:
-		
-		#intersect_id = intersection['intersect_id']
-		intersect_id = intersection['adm_id']
-		print intersect_id
-		#if intersect_id == 504:
-		geom = get_geoms(intersect_id, id_fieldname, table)
-		#geom = get_simple_geoms(intersect_id, id_fieldname, table)
-		dataset = 'umd-loss-gain'
-		
-		loss = gfw_api_request(geom, start_date, end_date, dataset, threshold=30)
-		#loss_field = 'loss_%s'% d
-		loss_field = 'total_loss_%s'% d
-		print loss_field
-		print loss
-		update_table(table, loss_field, loss, intersect_id, id_fieldname)
-		print orange
-		orange+=1
-
-def loss_ee_area(areaimg, theid,table, id_fieldname):
-	query = """
-        SELECT the_geom
-        FROM %s
-        WHERE %s = '%s'""" % (table,id_fieldname, theid)
-	r = requests.get(APIURL, params=dict(api_key=APIKEY, q=query, format='geojson'))
-	try:
-		geoms = r.json()['features'][0]['geometry']['coordinates']
-	#except TypeError:
-		# no geoms
-		#return None
-	except IndexError:
-		return None
-	geoms_sss = geoms[0][0]
-	feature = ee.Geometry.Polygon(geoms_sss)
-	#stats = areaimg.reduceRegion({'reducer': ee.Reducer.sum(),'geometry':feature,'maxPixels': 5e9})
-	stats = areaimg.reduceRegion(ee.Reducer.sum(), feature)
-
-	try:
-		area = stats.getInfo()['b1']
-		#area = stats.getInfo()['lossyear']
-		area_ha = area/ 10000
-		print area_ha
-		return area_ha
-	except ee.ee_exception.EEException:
-		#The region has too many pixels;
-		return 99999
-
-def raster_with_umd(rasterImg,year_s, year_e):
-	
-	umd = ee.Image('UMD/hansen/global_forest_change_2014')
-	treeCover = umd.select(['treecover2000'])
-	"""Create an image where pixels >=30 get the value 1 and all other pixels get the // value 0"""
-	treeExtent = treeCover.gte(30)
-	year = umd.select(['lossyear'])
-	"""Create an image where pixels between year_s and year_e get the value 1 and all other pixels get the // value 0"""
-	umdloss = year.gte(year_s).And(year.lte(year_e))
-	umdlossExtent = umdloss.And(treeExtent)
-	
-	loss_Img = rasterImg.And(umdlossExtent)
-	areaimg = loss_Img.multiply(ee.Image.pixelArea())
-	return areaimg
-
-def get_raster_area(raster, loss):
-	if raster == 'carbon':
-		image = ee.Image('GME/images/06900458292272798243-10265563443515643977')
-		"""Create an image where pixels of medium and high carbon stock get the value 1 and all other pixels get the value 0"""
-		"""HCS area of 40 Mg C/Ha"""
-		image_rcl = image.gte(40)
-
-	elif raster == 'primary':
-		image = ee.Image('GME/images/06900458292272798243-17233255607601779355')
-		"""Create an image where pixels of primary forest as of year 2000 get the value 1 and all other pixels get the value 0"""
-		image_rcl = image.neq(3)  #not equal to 3, 3 is the only value to exclude here
-
-	if loss== True:
-		areaImg = raster_with_umd(image_rcl, 1, 13)
-
-	elif loss== False:
-		areaImg = image_rcl.multiply(ee.Image.pixelArea())
-
-
-
-	return areaImg
-
-def loss_on_raster_in_district(table,dataset,loss,fieldname):
-	
-	areaimg = get_raster_area(dataset, loss)
-	
-	n = 441 #441 districts in Indonedsia, id_2 values ranging from 1 to 441
-	for id_2 in range(1,n+1):
-		print id_2
-		area = loss_ee_area(areaimg,id_2,table,fieldname)
-		if loss == False:
-			field = '%s_area' % dataset
-		elif loss == True:
-			field = 'loss_on_%s' % dataset
-		update_table(table, field, area,id_2, fieldname)
-		
-def loss_on_raster_in_intersections(table,dataset,loss):
-	areaimg = get_raster_area(dataset, loss)
-	id_fieldname = 'intersect_id'
-	ids = get_all_ids(table, id_fieldname)
-	for intersection in ids:
-		intersect_id = intersection['intersect_id']
-		print intersect_id
-		area = loss_ee_area(areaimg,intersect_id,table,id_fieldname)
-		field = '%s_area' % dataset
-		update_table(table, field, area,intersect_id, id_fieldname)
-
-def intersec_cal(table):
-	
-	i = 1
-	while(i <= 12):
-		c = str(2000 + i)
-		d = str(2000 + i + 1)
-		start_date = '%s-01-01' % c
-		end_date = '%s-01-01' % d
-		loss_in_geom(table, start_date, end_date, c)
-		i+=1
-
 def main():
 	#summarize_loss_in_intersec('idn_adm2_protected','loss_2001_2013','historic_protected_areas_loss')
-	#rate_loss_in_protected()
 	#palm_concession_in_protected('idn_adm2_protected','idn_adm2_protected_palm_concession','area_hectare','concession_in_protected')
 	#palm_concession_in_protected()
-	#intersec_cal('district_illegal_concession')
 	#palm_concession_in_protected('district_illegal_','district_illegal_concession','loss_2013','loss_concession_in_forest_estate')
 	#palm_concession_in_protected('idn_adm2_risk_analysis','district_peat_','area_hectare','peat_land_area')
-	#loss_in_geom('district_peat_union', '2001-01-01', '2014-01-01', '2001_2013')
 	
 	#palm_concession_in_protected('district_peat_','district_peat_union_concession','area_hectare','concession_on_peat')
-	
-	#loss_on_raster_in_district('idn_adm2_risk_analysis', 'primary', False, 'id_2')
-	#loss_on_raster_in_district('idn_adm2_risk_analysis', 'carbon', False, 'id_2')
-	#loss_on_raster_in_district('idn_adm2_risk_analysis','carbon', True, 'id_2')
-	#loss_on_raster_in_district('idn_adm2_risk_analysis','primary', True, 'id_2')
-	#loss_on_raster_in_district('idn_adm2_palm','primary', False, 'adm_id')
-	#loss_on_raster_in_district('idn_adm2_concession_mof','carbon', False, 'adm_id')
-	#loss_on_raster_in_intersections('idn_adm2_concession_mof_intersec_','carbon', False)
-
-
-
 	#intersec_cal('district_illegal_concession_mof')
 	#palm_concession_in_protected('district_illegal_','district_illegal_concession_mof','loss_2013','loss_concession_in_forest_estate')
 	#palm_concession_in_protected('idn_adm2_protected','idn_adm2_protected_palm_concession_mof','area_hectare','concession_in_protected')
 	#palm_concession_in_protected('idn_adm2_protected_buffer_10km','idn_adm2_protected_buffer_10km_palm_concession_mof','area_hectare','concession_in_protected_buffer_10km')
 
-	#intersec_cal('idn_adm2_protected')
 	#summarize_loss_in_intersec('idn_adm2_protected', 'loss_2001', 'loss_2001')
 	#summarize_loss_in_intersec('idn_adm2_protected', 'loss_2005', 'loss_2005')
 	#summarize_loss_in_intersec('idn_adm2_protected', 'loss_2006', 'loss_2006')
@@ -543,8 +317,6 @@ def main():
 	#regression('idn_adm2_risk_analysis', 'adm_id')
 
 	
-	#intersec_cal('district_peat_union')
-	#intersec_cal('idn_adm2_risk_analysis')
 	#regression_window('idn_adm2_risk_analysis')
 	#regression_window('district_peat_union')
 	regression_window('idn_prim_loss_0012_district_local')
